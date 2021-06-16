e918e3bd4292a8cec784d155c4b1533fb2c06973
louisreed73 <luis.sa1973@gmail.com>
2021-06-16 12:43:38 +0200

    refactor pipe

--- projects/app1/src/app/services/documento-detail.service.ts
+++ projects/app1/src/app/services/documento-detail.service.ts
@@ -3,27 +3,26 @@ import { Injectable, OnInit } from "@angular/core";
 import { of } from "rxjs";
 import { delay } from "rxjs/operators";
 
-import * as data  from "../../assets/pdf/pdfArray.json";
+import * as data from "../../assets/pdf/pdfArray.json";
 // const url="https://my-json-server.typicode.com/louisreed73/fakeAPIbuscador/documentos";
-const url="http://localhost:8080/bjus_rest_buscador/v1/documentos/";
+const url = "http://localhost:8080/bjus_rest_buscador/v1/documentos/";
 @Injectable({
   providedIn: "root",
 })
 export class DocumentoDetailService {
-  base64Src:any;
+  base64Src: any;
   constructor(private http: HttpClient) {
     // this.base64Src=(data as any).default.data;
-
     // console.log(this.base64Src)
   }
 
   getDocumentById(id: number) {
     console.log("Llamando el método getDocumentById!!!");
 
-    return this.http.get(`${url+id}`)
-    // .pipe(
-    //     delay(10000)
-    //   )
+    return this.http.get(`${url + id}`)
+    .pipe(
+      // delay(10000)
+      );
     // .subscribe(d=>{
     //   this.base64Src=(d as any).data.pdf;
     //   console.log(`%cFakeAPibuscador: ${JSON.stringify(d,null,3)}`,'color:gold')
@@ -40,6 +39,4 @@ export class DocumentoDetailService {
     //   delay(1250)
     // )
   }
-
-
 }
