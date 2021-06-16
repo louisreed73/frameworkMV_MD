aa967c70866ed5fe22ac2ea91b61203f2ac99021
louisreed73 <luis.sa1973@gmail.com>
2021-06-16 20:49:22 +0200

    more margin between coincidencias

--- projects/app1/src/app/sharedComponents/carousel/carousel.component.ts
+++ projects/app1/src/app/sharedComponents/carousel/carousel.component.ts
@@ -22,7 +22,7 @@ export class CarouselComponent implements OnInit {
     // navText: ["", ""],
     responsive: {
       0: {
-        items: 3.75,
+        items: 4,
       },
       500: {
         items: 7,
