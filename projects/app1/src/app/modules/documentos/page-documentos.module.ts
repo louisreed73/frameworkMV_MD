0f4f708d42f5e3a149910a2befac1565fcf582b9
louisreed73 <luis.sa1973@gmail.com>
2021-06-29 19:03:45 +0200

    documentation for new Pipe replaceUnderDash - implementation AuthGuard for default route

--- projects/app1/src/app/modules/documentos/page-documentos.module.ts
+++ projects/app1/src/app/modules/documentos/page-documentos.module.ts
@@ -6,9 +6,10 @@ import { SearchResolucionesComponent } from "projects/app1/src/app/pages/documen
 import { SearchEscritosComponent } from "projects/app1/src/app/pages/documentos/search-escritos/search-escritos.component";
 import { DocumentCardComponent } from "../../components/document-card/document-card.component";
 import { SharedUtilitiesModule } from "projects/app1/src/app/sharedModules/shared-utilities/shared-utilities.module";
+import { AuthGuard } from "@mova/components/core";
 
 const routes: Routes = [
-  { path: "documentos", component: SearchDocumentsComponent },
+  { path: "documentos", component: SearchDocumentsComponent, canActivate: [AuthGuard] },
   { path: "resoluciones", component: SearchResolucionesComponent },
   { path: "escritos", component: SearchEscritosComponent },
 ];
--- projects/app1/src/app/pipes/replaceUnderDash/replace-under-dash.pipe.ts
+++ projects/app1/src/app/pipes/replaceUnderDash/replace-under-dash.pipe.ts
@@ -1,10 +1,20 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
+/**
+ *
+ * ReplaceUnderDashPipe
+ * Pipe to transform
+ * strings of Filters Titles
+ * replacing "_" to ""
+ * Forced due to change model
+ * for API Buscador naming
+ *
+ */
 @Pipe({
   name: "replaceUnderDash",
 })
 export class ReplaceUnderDashPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
-    return value.split("").includes("_") ? value.replace("_"," ") : value + "";
+    return value.split("").includes("_") ? value.replace("_", " ") : value + "";
   }
 }
